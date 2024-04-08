326386e6f1e77efd6782e615642a5d32497a1680
Richard <richard232manansala@gmail.com>
2024-04-08 11:38:57 +0800

    Added observable for addresses

--- src/main/java/org/nice/models/UserModel.java
+++ src/main/java/org/nice/models/UserModel.java
@@ -4,10 +4,15 @@ import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.subjects.BehaviorSubject;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.UUID;
 
 public class UserModel {
-    private final ArrayList<Address> addresses = new ArrayList<>();
+    private final ArrayList<Address> addressesList = new ArrayList<>();
+
+    public Collection<Address> getAddressCollection() {
+        return addressesList;
+    }
 
     public Observable<Address> getMainAddress() {
         return mainAddress;
@@ -23,12 +28,14 @@ public class UserModel {
 
     private final BehaviorSubject<String> username = BehaviorSubject.createDefault("Username");
     private BehaviorSubject<Address> mainAddress;
+    private BehaviorSubject<Collection<Address>> addresses;
 
     public UserModel() {
-        addresses.add(
+        addressesList.add(
                 new Address("Your mama", "099392029", "lkjsdkljfa", UUID.randomUUID().toString())
         );
-        mainAddress = BehaviorSubject.createDefault(addresses.get(0));
+        mainAddress = BehaviorSubject.createDefault(addressesList.get(0));
+        addresses = BehaviorSubject.createDefault(addressesList);
     }
 
 }
